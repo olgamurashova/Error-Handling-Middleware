df51a9d217f4befb9d5d714809e6611db71fe5db
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-02-04 12:11:20 -0500

    Update errMiddleware.js

--- errMiddleware.js
+++ errMiddleware.js
@@ -1,4 +1,7 @@
-/*In this segment we assign the return value of the function possiblyProblematicOperation() to newValue. Then we check to see if this function returned anything at all. If it didn’t, we create a new Error and pass it to next(). This prompts the error-handling middleware to send a response back to the user, but many other error-handling techniques could be employed (like logging, re-attempting the failed operation, and/or emailing the developer).*/
+/*In this segment we assign the return value of the function possiblyProblematicOperation() to newValue.
+Then we check to see if this function returned anything at all. If it didn’t, we create a new Error and pass it to next(). 
+This prompts the error-handling middleware to send a response back to the user,
+but many other error-handling techniques could be employed (like logging, re-attempting the failed operation, and/or emailing the developer).*/
 
 app.use((req, res, next) => {
   const newvalue = PossiblyProblematicOperation();
