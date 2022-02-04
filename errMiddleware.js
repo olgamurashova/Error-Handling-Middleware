a898915289550249b9c0ff17b1a1a7b32af65601
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-02-04 12:07:24 -0500

    Update errMiddleware.js

--- errMiddleware.js
+++ errMiddleware.js
@@ -11,3 +11,12 @@ app.use((err, req, res, next) => {
   const status = err.status || 500;
   req.status(status).send(err.message);
 });
+
+
+//Alternative code
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+  
