62818d765d3432db16fe73df0c0298bcf85f3cbb
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-02-11 00:56:45 -0500

    Create readFile.js

---
+++ readFile.js
@@ -0,0 +1,12 @@
+app.get('/', [
+  function (req, res, next) {
+    fs.readFile('/maybe-valid-file', 'utf-8', function (err, data) {
+      res.locals.data = data
+      next(err)
+    })
+  },
+  function (req, res) {
+    res.locals.data = res.locals.data.split(',')[1]
+    res.send(res.locals.data)
+  }
+])
